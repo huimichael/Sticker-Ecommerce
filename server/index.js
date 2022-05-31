57c521f9d40229450124f1770c2f9fff311a60c2
Timothy Huo <61216890+timothyhuo1@users.noreply.github.com>
2022-05-31 12:33:27 -1000

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'sticker_system',
+});
+
+app.post('/create', (req, res) => {
+    const name = req.body.name;
+    const description = req.body.description;
+    const price = req.body.price;
+
+    db.query("INSERT INTO stickers (name, description, price) VALUES (?, ?, ?)",
+    [name, description, price],
+    (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send("values inserted")
+        }
+    });
+});
+
+app.get('/stickers', (req, res) => {
+    db.query("SELECT * FROM stickers", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    })
+})
+
+app.listen(3001, () => {
+    console.log('running on port 3001');
+})
